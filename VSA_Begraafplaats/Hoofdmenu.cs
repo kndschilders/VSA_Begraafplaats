306211251d22212f4e9785b47ea2943ac5e4644d
kndschilders <kndschilders@gmail.com>
2016-06-13 10:21:30 +0200

    Added hoofdmenu comments

--- VSA_Begraafplaats/Hoofdmenu.cs
+++ VSA_Begraafplaats/Hoofdmenu.cs
@@ -15,8 +15,9 @@ namespace VSA_Begraafplaats
 {
     public partial class Hoofdmenu : Form
     {
-        List<GraveLocation> graves = new List<GraveLocation>();
-
+        /// <summary>
+        /// Struct used to contain variables used for a maptext object.
+        /// </summary>
         private struct mapText
         {
             public string Text;
@@ -25,58 +26,92 @@ namespace VSA_Begraafplaats
             public bool Visible;
         }
 
-        mapText maptext = new mapText();
+        /// <summary>
+        /// A single mapText object. This object will be filled with the info of the
+        /// selected grave, and the location will be set to the location of the grave.
+        /// </summary>
+        private mapText maptext = new mapText();
 
         public Hoofdmenu()
         {
             InitializeComponent();
+
+            // Bind the mousewheel event to the mousewheel event handler
             this.pbxGraveyard.MouseWheel += pbxGraveyard_MouseWheel;
 
+            // Set the right settings on the datetimepicker
             dtpDate.Format = DateTimePickerFormat.Custom;
             dtpDate.CustomFormat = "dd/MMMM";
             dtpDate.ShowUpDown = true;
 
-            DataAccess.GetCemeteryInformation(1, DateTime.Now );
+            // Get the cemetery info
+            DataAccess.GetCemeteryInformation(1, DateTime.Now);
         }
 
+        /// <summary>
+        /// Logs the user in/out.
+        /// </summary>
+        /// <param name="loggedIn">The boolean indicating whether to log the user in or out.</param>
         public void SetLoggedIn(bool loggedIn)
         {
             if (loggedIn)
             {
                 // User is logged in
-            } else
-            {
+
+            } else {
                 // User  is logged out
+
             }
         }
 
+        /// <summary>
+        /// Mousewheel event handler used for zooming the map.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event object.</param>
         private void pbxGraveyard_MouseWheel(object sender, MouseEventArgs e)
         {
             MessageBox.Show("Scroll wheel used!\nButton:" + e.Delta);
         }
 
+        /// <summary>
+        /// Inloggen menu button event handler used for showing the login form.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event object.</param>
         private void inloggenToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Form loginForm = new Inloggen(this);
             loginForm.Show();
         }
 
+        /// <summary>
+        /// Trackbar year event hander used to show the gravelocations based on to year.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event object.</param>
         private void trbYear_ValueChanged(object sender, EventArgs e)
         {
             this.lblSliderYear.Text = Convert.ToString(this.trbYear.Value);
         }
 
+        /// <summary>
+        /// PictureBox graveyard mousemove event handler used to show the label when
+        /// the mouse is hovered over a grave.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event object.</param>
         private void pbxGraveyard_MouseMove(object sender, MouseEventArgs e)
         {
             this.lblMapCoords.Text = string.Format("{0},{1}",e.X,e.Y);
             this.maptext.Visible = false;
 
-            foreach (GraveLocation g in graves)
+            foreach (GraveLocation g in Controller.Cemetery.GraveLocations)
             {
                 if (e.X >= (g.Location.X - 10) && e.X <= (g.Location.X + 10) &&
                     e.Y >= (g.Location.Y - 10) && e.Y <= (g.Location.Y + 10))
                 {
-                    this.maptext.Text = "Grave #" + graves.IndexOf(g);
+                    this.maptext.Text = "Grave #" + Controller.Cemetery.GraveLocations.IndexOf(g);
                     this.maptext.X = (int)g.Location.X;
                     this.maptext.Y = (int)g.Location.Y;
                     this.maptext.Visible = true;
@@ -88,12 +123,17 @@ namespace VSA_Begraafplaats
             this.pbxGraveyard.Invalidate();
         }
 
+        /// <summary>
+        /// PictureBox graveyard click event handler used to create or show a grave.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event object.</param>
         private void pbxGraveyard_MouseClick(object sender, MouseEventArgs e)
         {
             // Check if mouse is hovering over a grave location
             GraveLocation grave = null;
 
-            foreach (GraveLocation g in graves)
+            foreach (GraveLocation g in Controller.Cemetery.GraveLocations)
             {
                 if (e.X >= (g.Location.X - 10) && e.X <= (g.Location.X + 10) &&
                     e.Y >= (g.Location.Y - 10) && e.Y <= (g.Location.Y + 10))
@@ -107,24 +147,38 @@ namespace VSA_Begraafplaats
             if (grave != null)
             {
                 //get cemetery from controller
-                //get grave spreads
-                //find grave spread with given grave location
+                Cemetery cemetery = Controller.Cemetery;
 
-                // Open grave gui
-                //Form form = new Graf();
-                //form.Show();
+                //get grave spreads
+                List<GraveSpread> graveSpreads = cemetery.GraveSpreads;
+
+                //find grave spread with given grave location
+                foreach (GraveSpread gs in graveSpreads)
+                {
+                    if (gs.GraveLocations.Find(gl => gl.ID == grave.ID) != null)
+                    {
+                        Form form = new Graf(gs);
+                        form.Show();
+                    }
+                }
             } else {
                 //graves.Add(new GraveLocation(e.X - 2, e.Y - 2));
                 MessageBox.Show("New Gravelocation");
                 //Form form = new Graf();
+                //form.Show();
 
                 this.pbxGraveyard.Invalidate();
             }
         }
 
+        /// <summary>
+        /// PictureBox graveyard paint event handler, used to paint the gravelocations.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event object.</param>
         private void pbxGraveyard_Paint(object sender, PaintEventArgs e)
         {
-            foreach (GraveLocation g in graves)
+            foreach (GraveLocation g in Controller.Cemetery.GraveLocations)
             {
                 e.Graphics.FillRectangle(new SolidBrush(Color.Red), g.Location.X, g.Location.Y, 5, 5);
             }
@@ -139,6 +193,11 @@ namespace VSA_Begraafplaats
             }
         }
 
+        /// <summary>
+        /// Graftool menuitem click event handler used to show the new reservation form.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event object.</param>
         private void grafToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Form newReservationForm = new NewReservation();
